93e1eb06ae7c2cdcdce9812328ec6c7ff9f6703b
Claudio <claudiocli.cp@gmail.com>
2022-02-18 11:00:45 +0100

    Added file handler support for minizinc
    Added `file_handler.py` to have a handler for the i/o for minizinc
    Implemented and tested `setup_config_file()`
    Updated `requirements.txt`

---
+++ core/file_handler.py
@@ -0,0 +1,31 @@
+# IMPORTANT!: file input of minizinc: 'piccione_viaggiatore.dzn'
+from typing import List
+
+
+FILE_CONFIG_NAME = 'piccione_viaggiatore.dzn'
+
+
+def setup_config_file(n: int, dist: List[List[int]], start_city: int, city_names: List[str]):
+    with open(FILE_CONFIG_NAME, 'w') as o:
+        o.write(f'n = {n};\n')
+        o.write('dist = [|\n')
+        for i in range(n):
+            for j in range(n):
+                if j == n - 1:
+                    o.write(f'{dist[i][j]}|\n')
+                else:
+                    o.write(f'{dist[i][j]},')
+        o.write('];')
+        o.write(f'\nstart_city = {start_city};')
+        o.write('\ncity_names = [')
+        for i in range(n):
+            if i == n - 1:
+                o.write(f'{city_names[i]}')
+            else:
+                o.write(f'{city_names[i]},')
+        o.write('];')
+
+
+def read_output_tsp():
+    # TODO: read the output of minizinc with the help of the python plugin for minizinc
+    pass
